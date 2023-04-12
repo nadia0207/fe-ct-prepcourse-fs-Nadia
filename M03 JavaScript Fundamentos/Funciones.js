5f8e704ca714ca12d90422ad547accd5f706bcf2
nadia0207 <nadia0207@hotmail.com>
2023-04-12 01:16:31 -0500

    primer commit PreHenry

---
+++ M03 JavaScript Fundamentos/Funciones.js
@@ -0,0 +1,20 @@
+/*
+//forma 1
+function sumaTres(x){
+    return x+3;
+}
+
+//forma 2
+var sumaTres1=function(x){
+    return x+3;
+}
+
+//forma 3
+var sumaTres2 = (x) =>{
+    return x+3;
+}*/
+
+function cuidadiConelConsoleLog(nombre){
+    console.log(nombre);
+    return nombre;
+}
---
+++ M03 JavaScript Fundamentos/variables.js
@@ -0,0 +1,23 @@
+var bootcamp= "Henry";
+var numero1 = 4;
+var numero2 = -4;
+var estoyenctiendo= true;
+var varible3;
+var sinnada= null;
+
+console.log(3 + 5 * 2 - 8);
+console.log('hola' + ' camilo');
+
+console.log (4 < 7); 
+
+console.log("estricto");
+console.log (3 == 3); 
+console.log (3 === 3); 
+console.log (3 == "3"); 
+console.log (3 === "3");
+console.log (7 == "7");
+console.log (7 === "7");
+
+
+
+
